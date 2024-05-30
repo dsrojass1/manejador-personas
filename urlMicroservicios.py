7d64f0b1110f1f3c0e82293cb3c402a03ee3b370
maurive2014 <111020376+maurive2014@users.noreply.github.com>
2024-05-30 14:09:54 -0500

    Update urlMicroservicios.py

--- urlMicroservicios.py
+++ urlMicroservicios.py
@@ -1,3 +1,3 @@
 import os
 
-PATH_AUTH = "http://" + os.environ.get("VARIABLES_HOST", "localhost") + ":" + os.environ.get("VARIABLES_PORT", "8080") + "/api/registrar/"
+PATH_AUTH = "http://" + os.environ.get("VARIABLES_HOST", "10.128.0.8") + ":" + os.environ.get("VARIABLES_PORT", "8080") + "/api/registrar/"
